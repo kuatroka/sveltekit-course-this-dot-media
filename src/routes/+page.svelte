0c0aebdbb6d5b543ec0b629cfde998ae84191f32
kuatroka <aaarmani_2000@yahoo.com>
2023-11-24 23:46:07 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import * as Card from "$lib/components/ui/card";
+</script>
+
+<div class="w-full flex flex-auto mt-4 gap-2 md:flex-row">
+
+<Card.Root class="w-full">
+    <Card.Header>
+        <Card.Title><h1>Card Title</h1>
+        </Card.Title>
+        <Card.Description>
+            <p>Card Description</p>
+        </Card.Description>
+    </Card.Header>
+    <Card.Content>
+        <p>Card Content</p>
+    </Card.Content>
+    <Card.Footer>
+        <p>Card Footer</p>
+    </Card.Footer>
+</Card.Root>
+
+<Card.Root class="w-full">
+    <Card.Header>
+        <Card.Title><h1>Card Title</h1>
+        </Card.Title>
+        <Card.Description>
+            <p>Card Description</p>
+        </Card.Description>
+    </Card.Header>
+    <Card.Content>
+        <p>Card Content</p>
+    </Card.Content>
+    <Card.Footer>
+        <p>Card Footer</p>
+    </Card.Footer>
+</Card.Root>
+</div>
